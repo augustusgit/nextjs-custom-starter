e990fa982115580871013140669586894e1294bc
west <augustusnwangwu@gmail.com>
2023-10-11 03:41:11 +0100

    Register forn UI

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,27 @@
+import {useState} from 'react'
+
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //
+    };
+
     return (
         <>
         <h1 className="jumbotron text-center bg-primary square" >Register</h1>
-        <p>register page</p>
+        <div className="container col-md-4 offset-md-4 pb-5 ">
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="form-control mb-4 p-4" value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Name' required />
+                <input type="email" className="form-control mb-4 p-4" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter Email' required />
+                <input type="password" className="form-control mb-4 p-4" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter Password' required />
+                <br/>
+                <button type='submit' className='btn btn-block btn-primary'>submit</button>
+            </form>
+        </div>
         </>
     )
 }
